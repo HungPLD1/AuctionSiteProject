7b10108deb25be821a77caeb064a3847f29b90e4
Truong Quang Hieu <quanghieu.truong.fr@gmail.com>
2019-12-08 16:40:39 +0700

    fix bidlogs.go

--- controller/bidlogs.go
+++ controller/bidlogs.go
@@ -24,7 +24,7 @@ func BidLogs(c *gin.Context) {
 		Select("user_id, bid_amount, bid_date").
 		Scan(&logs).
 		Error
-	if errgetLogs != nil {q
+	if errgetLogs != nil {
 		log.Println(errgetLogs)
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error":   errgetLogs,
